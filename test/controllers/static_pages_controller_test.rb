3235bba751fb58bfff4c0a0ce4b84b67f64a4f6a
Vitalii <iksel@ukr.net>
2022-11-29 15:36:10 +0200

    solved v1.1 exercise 3.1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,18 +7,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | 5" + @base_row
+    assert_select "title", "Home | " + @base_row
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | 5#{@base_row}"
+    assert_select "title", "Help | #{@base_row}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | 5Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
