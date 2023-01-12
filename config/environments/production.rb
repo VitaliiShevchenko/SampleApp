4848827a509be74470e89939d659d9c34e65f919
Vitalii <iksel@ukr.net>
2023-01-12 21:48:14 +0200

    change production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   #host = 'https://lit-sands-61362.herokuapp.com'
-  host = 'https://cp-shevchenko.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'cp-shevchenko.herokuapp.com'
+  config.action_mailer.default_url_options = { protocol: 'http', host:  host }
   ActionMailer::Base.smtp_settings = {
     :address  => 'smtp.sendgrid.net',
-      :port  => '587',
+      :port  => 587,
     :authentication  => :plain,
     :user_name  => ENV['SENDGRID_USERNAME'],
   :password  => ENV['SENDGRID_PASSWORD'],
