28714e8499f9ee5fe12edeb534df0a4d2f43df1c
Vitalii <iksel@ukr.net>
2023-01-13 08:59:58 +0200

    mistake in login def &.authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@
 
   def create
     @user = User.find_by(email: params[:session][:email])
-    if @user &.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       if @user.activated?
         log_in @user
         params[:session][:remember_me] === "1" ? remember(@user) : forget(@user)
