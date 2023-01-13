586832c6a7c3a61b052aff0ee575d029cb7fe465
Vitalii <iksel@ukr.net>
2023-01-14 01:17:04 +0200

    change a place jqery over pin application

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js", preload: true
 pin "application",  preload: true
     pin "@hotwired/turbo-rails", to: "turbo.min.js"
     pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,7 +8,7 @@ pin "application",  preload: true
 # pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js", preload: true
+
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 
