db97796ad75b11e64871d8235be1f43674430049
Vitalii <iksel@ukr.net>
2023-01-13 17:15:12 +0200

    change PictureUploader for verify on HEROKU

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,12 +6,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
+  #delete this by real production. This is only for verify on HEROKU
+  storage :file
+
+  #uncomment below be real deploy. This is only for verify on HEROKU
+  #if Rails.env.productn?
+  #  storage :fog
+  #else
   # Choose what kind of storage to use for this uploader:
-    storage :file
-  end
+  #  storage :file
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
